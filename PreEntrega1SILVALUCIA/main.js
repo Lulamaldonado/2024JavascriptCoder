7f0a0b095c1f8a50ba9efdd176b9c9ed259389ef
SoyDelivery <lucia.silva@soydelivery.com.uy>
2024-09-18 15:59:28 -0300

    correccion1

--- PreEntrega1SILVALUCIA/main.js
+++ PreEntrega1SILVALUCIA/main.js
@@ -2,9 +2,47 @@ function calcularPreciofinal(precio,iva){
     return precio+(precio*iva/100)
 }
 
-let precio= parseFloat (prompt ("Ingresar precio producto: "));
+let precio= parseFloat (prompt ("Ingresar precio championes: "));
 
 let iva= 22;
 let precioTotal= calcularPreciofinal( precio, iva);
 
 alert ("El precio final es : "+ precioTotal)
+
+
+
+
+
+
+
+
+
+
+/*let precio= prompt ("Ingresar precio del calzado elegido: ");
+
+switch(precio){
+    case "Nike Mercurial":
+        Nike Mercurial()
+        break; 
+    case "Nike Tiempo Emerald":
+        Nike Tiempo Emerald()
+        break; 
+
+        function calcularPreciofinal(precio,iva){
+            return precio+(precio*iva/100)
+    
+        }
+
+let iva= 22;
+
+let precioTotal= calcularPreciofinal( precio, iva);
+
+alert ("El precio a abonar es : "+ precioTotal) */
+
+
+
+
+
+
+
+

1259982d8a0513d24beefd2621c8ef16ca5d6464
SoyDelivery <lucia.silva@soydelivery.com.uy>
2024-09-28 21:12:11 -0300

    Prentega 2Silva

--- PreEntrega1SILVALUCIA/main.js
+++ PreEntrega1SILVALUCIA/main.js
@@ -1,13 +1,31 @@
-function calcularPreciofinal(precio,iva){
-    return precio+(precio*iva/100)
+// function calcularPreciofinal(precio,iva){
+    // return precio+(precio*iva/100)
+// }
+
+// let precio= parseFloat (prompt ("Ingresar precio championes: "));
+
+// let iva= 22;
+// let precioTotal= calcularPreciofinal( precio, iva);
+
+// alert ("El precio final es : "+ precioTotal)
+
+const calzado = [
+{ id:2 , nombre:"Nike Mercurial" ,precio:5500},
+{ id:3 , nombre:"Nike Tiempo Emerald" ,precio:4500}
+] 
+
+const productoBuscado= prompt (" Que calzado deseas buscar?");
+
+const productoEncontrado= calzado.find (producto => producto.nombre.toLocaleLowerCase()=== productoBuscado.toLocaleLowerCase());
+
+if (productoEncontrado){
+    alert (`El precio del calzado es ${productoEncontrado.precio}`);
+} else {
+    alert("Precio no encotrado");
+
 }
 
-let precio= parseFloat (prompt ("Ingresar precio championes: "));
 
-let iva= 22;
-let precioTotal= calcularPreciofinal( precio, iva);
-
-alert ("El precio final es : "+ precioTotal)
 
 
 
